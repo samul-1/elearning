124cf1d457a9422952af632fe35bdd4a085eebeb
samul-1 <bsamuele00@gmail.com>
2021-02-23 17:07:02 +0100

    testing on DO

--- elearning/settings.py
+++ elearning/settings.py
@@ -120,10 +120,9 @@ WSGI_APPLICATION = "elearning.wsgi.application"
 
 # ! ONLY FOR TEST ON DO
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL", False), conn_max_age=600
+    )
 }
 
 
