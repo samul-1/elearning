089a2bd54e77f23481b86a8086544c2211bc82df
samul-1 <bsamuele00@gmail.com>
2021-02-23 16:47:58 +0100

    added Procfile

--- elearning/settings.py
+++ elearning/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", None)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "apps.bsamu.it"]
 
 
 # Application definition
