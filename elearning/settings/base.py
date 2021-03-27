54b1159bb9fa89656ef7f4924efcf731ed057beb
samul-1 <bsamuele00@gmail.com>
2021-03-27 13:54:16 +0100

    added admin maling for error reporting in production

--- elearning/settings/base.py
+++ elearning/settings/base.py
@@ -75,35 +75,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "elearning.urls"
 
-# ------
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "handlers": {
-#         "console": {"class": "logging.StreamHandler"},
-#         "mail_admins": {
-#             "level": "ERROR",
-#             "class": "django.utils.log.AdminEmailHandler",
-#             "include_html": True,
-#         },
-#     },
-#     "loggers": {
-#         "django": {
-#             "handlers": ["console"],
-#             "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
-#         }
-#     },
-# }
-
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = os.environ.get("GMAIL_APP_ADDRESS", "bsamuele00@gmail.com")
-EMAIL_HOST_PASSWORD = os.environ.get("GMAIL_APP_PWD", "kcowwwteitnyavcf")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-# ------
 
 TEMPLATES = [
     {
